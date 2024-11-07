6465fee9a47e0f1ea6ce0aa797996ee6cf0af438
xmjblog <xmjuers@246801.xyz>
2024-11-07 21:53:22 +0800

    Site updated: 2024-11-07 21:53:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/30/hello-world/"];function toRandomPost(){
+var posts=["posts/16107/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
